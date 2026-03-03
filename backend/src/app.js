dadfd4d8617b0139cc50c2e5df2cc3e0ebf85356
Harshit Kandpal <kandpalhar@gmail.com>
2026-03-04 00:39:54 +0530

    Added cookie parser for refresh token

--- backend/src/app.js
+++ backend/src/app.js
@@ -23,7 +23,7 @@ app.use(cors({
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
-
+app.use(cookieParser());
 // API routes
 // app.use("/api/healthcheck", heathcheckRouter);
 app.use("/api/users", userRouter);
